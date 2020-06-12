cd2107f959ad4d6a7f95771597ba9b2ef314491f
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-06-12 02:50:29 +0100

    removed unused var

--- generate_csv.php
+++ generate_csv.php
@@ -1,21 +1,16 @@
 <?php
 
-
-function generateCSVFile($heading_array,$data_array)
+function generateCSVFile($heading_array, $data_array)
 {
-
-
-header('Content-Type: application/excel');
-header('Content-Disposition: attachment; filename="mailinglist.csv"');
-//initiate file download
-$fp = fopen('php://output', 'wb');
- fputcsv($fp, $heading_array, ',');
-$user_CSV= [];
-foreach ($data_array as $data_row) {
- fputcsv($fp, $data_row, ',');
-}
-fclose($fp);
-
+    header('Content-Type: application/excel');
+    header('Content-Disposition: attachment; filename="mailinglist.csv"');
+    //initiate file download
+    $fp = fopen('php://output', 'wb');
+    fputcsv($fp, $heading_array, ',');
+    foreach ($data_array as $data_row) {
+        fputcsv($fp, $data_row, ',');
+    }
+    fclose($fp);
 }
 /*
 Use case
