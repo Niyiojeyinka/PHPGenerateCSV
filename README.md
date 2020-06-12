47126e2c4250fe638cabdaa6d25dfb1cb5b0e9c7
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-06-12 02:52:33 +0100

    added third parameter filename

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 #PHPGenerateCSV : Generate CSV File from two parameter array
-where the first parameter is the title(S) while the second parameter is 
-the array of data of th csv 
+where the first parameter is the title(S) while the second parameter is
+the array of data of th csv and third the parmeter is the file name
 check example below
+
 ```php
 <?php
 //Use case
-$heading_array =array('firstname','lastname');
-$data = array(array('niyi','philip'),array('ola','dare'));
+$heading_array = ['firstname', 'lastname'];
+$data = [['niyi', 'philip'], ['ola', 'dare']];
 
-generateCSVFile($heading_array,$data);
+generateCSVFile($heading_array, $data, 'myfilename');
 ?>
 ```
--- generate_csv.php
+++ generate_csv.php
@@ -1,9 +1,9 @@
 <?php
 
-function generateCSVFile($heading_array, $data_array)
+function generateCSVFile($heading_array, $data_array, $filename)
 {
     header('Content-Type: application/excel');
-    header('Content-Disposition: attachment; filename="mailinglist.csv"');
+    header('Content-Disposition: attachment; filename="' . $filename . '.csv"');
     //initiate file download
     $fp = fopen('php://output', 'wb');
     fputcsv($fp, $heading_array, ',');
@@ -17,6 +17,6 @@ Use case
 $heading_array =array('firstname','lastname');
 $data = array(array('niyi','philip'),array('ola','dare'));
 
-generateCSVFile($heading_array,$data);
+generateCSVFile($heading_array,$data,'mycsvfilename');
 */
 ?>
